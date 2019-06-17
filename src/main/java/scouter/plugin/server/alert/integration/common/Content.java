2d5aa5f9ba12171702b3ed9517b0c17119040b26
서영준 <yj.seo@wemakeprice.com>
2019-06-17 14:20:35 +0900

    add scouter hostname to message

--- src/main/java/scouter/plugin/server/alert/integration/common/Content.java
+++ src/main/java/scouter/plugin/server/alert/integration/common/Content.java
@@ -2,6 +2,7 @@ package scouter.plugin.server.alert.integration.common;
 
 import scouter.lang.AlertLevel;
 import scouter.lang.pack.AlertPack;
+import scouter.server.Configure;
 
 public class Content {
 
@@ -10,6 +11,7 @@ public class Content {
     private String level;
     private String title;
     private String message;
+    private String scouterHostName;
 
     public Content(AlertPack pack, String objType, String objName) {
         this.type = objType;
@@ -17,11 +19,13 @@ public class Content {
         this.level = AlertLevel.getName(pack.level);
         this.title = pack.title;
         this.message = pack.message;
+        this.scouterHostName = Configure.getInstance().server_id;
 
     }
 
     public String toString() {
-        return "[TYPE] : " + this.type + "\n" +
+        return "[SCOUTER] : " + this.scouterHostName + "\n" +
+            "[TYPE] : " + this.type + "\n" +
             "[NAME] : " + this.name + "\n" +
             "[LEVEL] : " + this.level + "\n" +
             "[TITLE] : " + this.title + "\n" +
