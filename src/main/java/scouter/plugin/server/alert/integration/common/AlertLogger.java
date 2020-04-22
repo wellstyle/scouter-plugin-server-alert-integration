5ae1f0ee6ce454ff94f61f154840ca0c39b26db3
서영준 <yj.seo@wemakeprice.com>
2020-04-22 16:50:39 +0900

    improve AlertLogger

--- src/main/java/scouter/plugin/server/alert/integration/common/AlertLogger.java
+++ src/main/java/scouter/plugin/server/alert/integration/common/AlertLogger.java
@@ -9,20 +9,20 @@ import scouter.server.Logger;
 public class AlertLogger {
 
     private static final String EXT_PLUGIN_ALERT_DEBUG = "ext_plugin_alert_debug";
+    private static final String LOG_PREFIX = "[ext_plugin_alert] ";
 
     private boolean debug;
 
     public AlertLogger() {
         Configure conf = Configure.getInstance();
-        this.debug = conf.getBoolean(EXT_PLUGIN_ALERT_DEBUG, true);
+        this.debug = conf.getBoolean(EXT_PLUGIN_ALERT_DEBUG, false);
     }
 
     public void println(String message) {
         String threadName = "[" + Thread.currentThread().getName() + "] ";
 
         if (debug) {
-            Logger.println(threadName + message);
-            System.out.println(threadName + message);
+            Logger.println(LOG_PREFIX + threadName + message);
         }
     }
 }
